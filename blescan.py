16051f8a66729bd61d57343a1d6f1a6627bfcf0b
skrud4268 <41458731+skrud4268@users.noreply.github.com>
2018-09-18 18:13:54 +0900

    Update blescan.py

--- blescan.py
+++ blescan.py
@@ -163,7 +163,7 @@ def parse_events(sock, loop_count=100):
 		    # build the return string
                     Adstring = returnstringpacket(pkt[report_pkt_offset -22: report_pkt_offset - 6]) 
 		    Adstring += ","
-		    Adstring += Adstring = packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
+		    Adstring += packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
 		    Adstring += ","
 		    Adstring += "%i" % returnnumberpacket(pkt[report_pkt_offset -6: report_pkt_offset - 4]) 
 		    Adstring += ","
